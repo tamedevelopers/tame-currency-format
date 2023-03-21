80385db070dcfde8e756b79262cf952bd05e0885
Fredrick Peter <tamdevelopers@gmail.com>
2023-03-21 12:34:20 +0100

    First version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,347 @@
 # tame-currency-format
-Javascript/Jquery Currency Format
+### @author Fredrick Peterson (Tame Developers)
+### 2023 Fredrick Peterson
+### Intl.NumberFormat
+
+Javascript/JQuery Plugin for Number/Currency Formatting
+```
+|--------------------------------------------------------------------------
+|This Library uses the Javascript Intl.NumberFormat
+|https://github.com/bachors/PHP-Image-Compressor-Class
+```
+[Intl.NumberFormat](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat)
+
+* [Installation](#installation)
+* [Object Properties](#object-properties)
+* [Usage](#usage)
+  * [JQuery](#Jquery)
+  * [JQuery Bind To Element](#Jquery-bind-to-element)
+  * [JQuery Global Currency](#Jquery-global-currency)
+  * [Javascript](#javascript)
+  * [Javascript Bind To Element](#javascript-bind-to-element)
+  * [Javascript Global Currency](#javascript-global-currency)
+* [Currency Codes](#currency-codes)
+* [Useful links](#useful-links)
+
+
+## Installation
+Not avaialable on npm yet.
+
+**include any of the files** `You can clone or download the repo`:
+
+```
+<script src="../JQuery/tameCurrencyFormat.min.js"></script>
+<script src="../js/tameCurrencyFormat.min.js"></script>
+```
+
+
+## Object Properties
+``` 
+`ALL objected properties and values`
+
+    | Key               | Default   | Accepts-Data          |  Description                                      |
+    _____________________________________________________________________________________________________________
+    | currency          | HKD       | string                | Currency Code (refer to `currency code` section)  |
+    | currency_symbol   | empty     | string|true|false     | You can provide your preffered currency symbol    |
+    | display           | true      | true|false            | Display currency symbol                           |
+    | space             | true      | true|false            | Alow space between currency and formatted number  |
+    | position          | false     | false|'left'|'right'  | Currency symbol possition                         |
+    | number            | 0         | int|string            | Numbers to format                                 |
+    | decimals          | 2         | 0-4                   | Decimal points formating (from 1 to 4 decimals)   |
+```
+
+
+## JQuery
+
+**Example 1** — `JQuery`:
+```
+    $.tameCurrency.format({
+        currency: 'TRY',
+        number: 8.99,
+    });
+```
+
+**Example 2** — `JQuery`:
+```
+    $.tameCurrency.format({
+        currency: 'TRY',
+        currency_symbol: 'TL',
+        number: 8.99,
+        position: 'right',
+    });
+```
+
+## JQuery Bind To Element
+```
+Can only be used on any DOM elements that text can be changed.
+You can not use direct on 'INPUTS' elements
+```
+
+**Example 1** — `JQuery Bind To Element`:
+```
+    $('.element').tameCurrency.format({
+        currency: 'TRY',
+        number: 8.99,
+    });
+```
+
+## JQuery Global Currency
+
+```
+Need to be called above before using the `format()` function.
+When used, it replace every other call except you override with `currency_symbol` object
+```
+
+**Example 1** — `JQuery Global Currency`:
+```
+    $.tameCurrency.init('$USD');
+```
+
+```
+    var test1 = $.tameCurrency.format({
+        currency: 'TRY',
+        number: 8.99,
+    });
+
+    console.log(test1); // $USD 8,99
+
+    var test2 = $.tameCurrency.format({
+        currency: 'EUR',
+        currency_symbol: true,
+        number: 10.99,
+    });
+
+    console.log(test2); // 10,99 €
+```
+
+
+
+## Javascript
+
+**Example 1** — `Javascript`:
+```
+    tameCurrency.format({
+        currency: 'TRY',
+        number: 8.99,
+    });
+```
+
+**Example 2** — `Javascript`:
+```
+    tameCurrency.format({
+        currency: 'TRY',
+        currency_symbol: 'TL',
+        number: 8.99,
+        position: 'right',
+    });
+```
+
+## Javascript Bind To Element
+```
+Can only be used on any DOM elements that text can be changed.
+You can not use direct on 'INPUTS' elements
+```
+
+**Example 1** — `Javascript Bind To Element`:
+```
+    var element = document.querySelector('.element');
+
+    tameCurrency.input(element, {
+        currency: 'TRY',
+        number: 8.99,
+    });
+```
+
+## Javascript Global Currency
+
+```
+Need to be called above before using the `format()` function.
+When used, it replace every other call except you override with `currency_symbol` object
+```
+
+**Example 1** — `Javascript Global Currency`:
+```
+    tameCurrency.init('$USD');
+```
+
+```
+    var test1 = tameCurrency.format({
+        currency: 'TRY',
+        number: 8.99,
+    });
+
+    console.log(test1); // $USD 8,99
+
+    var test2 = tameCurrency.format({
+        currency: 'EUR',
+        currency_symbol: true,
+        number: 10.99,
+    });
+
+    console.log(test2); // 10,99 €
+```
+
+## Currency Codes
+```
+    {
+        AED: "ar-AE"
+        AFN: "ps"
+        ALL: "sq"
+        AMD: "hy"
+        ANG: "nl"
+        AOA: "pt"
+        ARS: "es-AR"
+        AUD: "en-AU"
+        AWG: "nl-AW"
+        AZN: "az-Latn"
+        BAM: "bs-Latn"
+        BBD: "en-BB"
+        BDT: "bn"
+        BGN: "bg"
+        BHD: "ar-BH"
+        BIF: "fr-BI"
+        BMD: "en-BM"
+        BND: "ms"
+        BOB: "es-BO"
+        BRL: "pt-BR"
+        BSD: "en-BS"
+        BTN: "dz"
+        BWP: "en-BW"
+        BYN: "be"
+        BZD: "en-BZ"
+        CAD: "en-CA"
+        CDF: "fr-CD"
+        CHF: "fr-CH"
+        CLP: "es-CL"
+        CNY: "zh-CN"
+        COP: "es-CO"
+        CRC: "es-CR"
+        CUC: "es"
+        CVE: "pt"
+        CZK: "cs"
+        DJF: "fr-DJ"
+        DKK: "da"
+        DOP: "es-DO"
+        DZD: "ar-DZ"
+        EGP: "ar-EG"
+        ERN: "ti-ET"
+        ETB: "am"
+        EUR: "de-DE"
+        FJD: "en-FJ"
+        GBP: "en-GB"
+        GEL: "ka"
+        GHS: "ak"
+        GIP: "en-GI"
+        GMD: "en"
+        GNF: "fr-GN"
+        GTQ: "es-GT"
+        GYD: "en-GY"
+        HKD: "zh-Hant-HK"
+        HNL: "es-HN"
+        HRK: "hr"
+        HTG: "fr-HT"
+        HUF: "hu"
+        IDR: "id"
+        ILS: "he"
+        INR: "hi"
+        IQD: "ar-IQ"
+        IRR: "fa-IR"
+        ISK: "is"
+        JMD: "en-JM"
+        JOD: "ar-JO"
+        JPY: "ja-JP"
+        KES: "sw-KE"
+        KGS: "ky"
+        KHR: "km"
+        KMF: "ar"
+        KPW: "ko-KP"
+        KRW: "ko-KR"
+        KWD: "ar-KW"
+        KYD: "en-KY"
+        KZT: "kk"
+        LAK: "lo"
+        LBP: "ar-LB"
+        LKR: "si"
+        LRD: "en-LR"
+        LSL: "st"
+        LYD: "ar-LY"
+        MAD: "ar-MA"
+        MDL: "ro"
+        MGA: "mg"
+        MKD: "mk"
+        MMK: "my"
+        MNT: "mn-Cyrl"
+        MOP: "zh-Hant"
+        MRU: "ar-MR"
+        MUR: "fr-MU"
+        MVR: "dv"
+        MWK: "ny"
+        MXN: "es-MX"
+        MYR: "ms-MY"
+        MZN: "pt-MZ"
+        NAD: "en-NA"
+        NGN: "ig"
+        NIO: "es-NI"
+        NOK: "nb"
+        NPR: "ne"
+        NZD: "en-NZ"
+        OMR: "ar-OM"
+        PAB: "es-PA"
+        PEN: "es-PE"
+        PGK: "tpi"
+        PHP: "fil"
+        PKR: "ur"
+        PLN: "pl"
+        PYG: "gn"
+        QAR: "ar-QA"
+        RON: "ro"
+        RSD: "sr-Latn"
+        RUB: "ru"
+        RWF: "rw"
+        SAR: "ar-SA"
+        SBD: "en-SB"
+        SCR: "fr-SC"
+        SDG: "ar-SD"
+        SEK: "sv-SE"
+        SGD: "zh-Hans-SG"
+        SHP: "en-SH"
+        SLL: "kri"
+        SOS: "sw"
+        SRD: "nl-SR"
+        SSP: "ar-SS"
+        STN: "pt-ST"
+        SYP: "ar-SY"
+        SZL: "en-SZ"
+        THB: "th"
+        TJS: "tg"
+        TMT: "tk"
+        TND: "ar-TN"
+        TOP: "to"
+        TRY: "tr-TR"
+        TTD: "en-TT"
+        TWD: "zh-Hant-TW"
+        TZS: "sw-TZ"
+        UAH: "uk"
+        UGX: "sw-UG"
+        USD: "en-US"
+        UYU: "es-UY"
+        UZS: "uz-Latn"
+        VEF: "es-VE"
+        VND: "vi"
+        VUV: "bi"
+        WST: "sm"
+        XAF: "fr"
+        XCD: "en"
+        XOF: "fr"
+        YER: "ar-YE"
+        ZAR: "zu"
+        ZMW: "en-ZM"
+    }
+```
+
+
+## Useful links
+
+- If you love this Javascript/JQuery Plugin, you can [Buy Tame Developers a coffee](https://www.buymeacoffee.com/tamedevelopers)
+- More examples in the test folder
+
---
+++ jquery/tameCurrencyFormat.js
@@ -0,0 +1,174 @@
+/*!
+ * Tame Currency Format v1.0.0
+ * https://tamedevelopers.com
+ * https://tametemplates.com
+ * Copyright 2023 Fredrick Peterson
+ * Released under the MIT license
+ */
+
+(function($) {
+    "use strict";
+
+    const __tameCurrencyFormat = function(options) {
+        var countryCurrency = __tameGetCountryCurrencies();
+
+        // check if currency symbol is passed
+        function __tameCheckCurrencySymbol(string){
+            if ((typeof string === 'string' && $.trim(string).length > 0) || string === true) {
+                return true;
+            }
+            return false;
+        };
+    
+        // get format type
+        function __tameGetStyleFormat(style){
+            if ($.trim(style).toLowerCase() === 'currency') {
+                return style;
+            }else if($.trim(style).toLowerCase() === 'decimal'){
+                return style;
+            }
+            return 'currency';
+        };
+    
+        // configure global default currency symbol
+        function __tameGetCurrencySymbol(options){
+            if(window.hasOwnProperty('tameCurrencySymbol__')){
+                // only override - if custom currency symbol is not set\for each call
+                if(!__tameCheckCurrencySymbol(settings.currency_symbol)){
+                    return window.tameCurrencySymbol__;
+                }
+            }
+            return options.currency_symbol;
+        };
+
+        // extend to settings var
+        var settings = $.extend({
+            style: 'currency',
+            currency: '',
+            currency_symbol: '',
+            display: true,
+            space: true,
+            position: false,
+            number: 0,
+            decimals: 2,
+        }, options);
+
+        // currency currency_symbol
+        settings.currency_symbol = __tameGetCurrencySymbol(settings);
+
+        // format style
+        settings.style = __tameGetStyleFormat(settings.style);
+    
+        // convert currency to uppercase
+        settings.currency = settings.currency.toUpperCase();
+    
+        // Check if currency exist in the object 
+        if(!countryCurrency.hasOwnProperty(settings.currency)){
+            settings.currency = 'HKD';
+        }
+    
+        // get currency value from - countryCurrency (object)
+        settings.language = countryCurrency[settings.currency] || 'en-US';
+    
+        // if symbol display of currency is not true
+        if (!settings.display) {
+            settings.style = 'decimal';
+
+            // if currency display not true, then we clean in case it was passed
+            settings.currency_symbol = '';
+        }else{
+            if(settings.style == 'decimal'){
+                settings.currency_symbol = '';
+                settings.display = false;
+            }
+        }
+
+        var intNumber = new Intl.NumberFormat(settings.language, {
+            style: settings.style,
+            currency: settings.currency,
+            minimumFractionDigits: settings.decimals > 4 ? 4 : settings.decimals,
+            maximumFractionDigits: 4,
+            useGrouping: true,
+        });
+
+        // get amount 
+        var formattedNumber = intNumber.format(settings.number);
+
+        // get an array of formatted parts of the number
+        var matches = intNumber.formatToParts(settings.number);
+
+        // only try if currency display is allowed
+        if(settings.display){
+            // Extract currency symbol
+            matches = intNumber.formatToParts(settings.number).find(function(part) {
+                return part.type === "currency";
+            }).value;
+
+            // if custom currency symbol is not set\ Collect symbol from matches array
+            if(!__tameCheckCurrencySymbol(settings.currency_symbol) || settings.currency_symbol === true){
+                settings.currency_symbol = matches;
+            }
+        }   
+
+        // trim string
+        formattedNumber = formattedNumber.replace(/\s/g, '');
+
+        // if display is true
+        if (settings.display) {
+        
+            // if space is allowed
+            if (settings.space) {
+                if(settings.position === false){
+                    if (formattedNumber.startsWith(matches)) {
+                        formattedNumber = formattedNumber.replace(matches, settings.currency_symbol + " ");
+                    } else{
+                        formattedNumber = formattedNumber.replace(matches, " " + settings.currency_symbol);
+                    }
+                }else{
+                    // replace currency symbol found  with (empty string)
+                    formattedNumber = formattedNumber.split(matches).join('');
+                    if(settings.position == 'left'){
+                        formattedNumber = settings.currency_symbol + ' ' + formattedNumber;
+                    }else{
+                        formattedNumber = formattedNumber + ' ' + settings.currency_symbol;
+                    }
+                }
+            } else{
+                if(settings.position === false){
+                    formattedNumber = formattedNumber.replace(matches, settings.currency_symbol);
+                }else{
+                    // replace currency symbol found  with (empty string)
+                    formattedNumber = formattedNumber.split(matches).join('');
+                    if(settings.position == 'left'){
+                        formattedNumber = settings.currency_symbol + formattedNumber;
+                    }else{
+                        formattedNumber = formattedNumber + settings.currency_symbol;
+                    }
+                }
+            }
+        }
+        
+        return formattedNumber;
+    };
+
+    // init default currency symbol
+    const __tameCurrencyFormatInit = function(currency = 'HK$'){
+        window.tameCurrencySymbol__ = currency;
+    };
+
+    // create countries currency `code` and `local-code`
+    const __tameGetCountryCurrencies = function(){
+        return {"USD":"en-US","EUR":"de-DE","JPY":"ja-JP","GBP":"en-GB","AUD":"en-AU","CAD":"en-CA","CNY":"zh-CN","TRY":"tr-TR","AFN":"ps","ALL":"sq","DZD":"ar-DZ","AOA":"pt","ARS":"es-AR","AMD":"hy","AWG":"nl-AW","AZN":"az-Latn","BSD":"en-BS","BHD":"ar-BH","BDT":"bn","BBD":"en-BB","BYN":"be","BZD":"en-BZ","XOF":"fr","BMD":"en-BM","BTN":"dz","BOB":"es-BO","BAM":"bs-Latn","BWP":"en-BW","BRL":"pt-BR","BND":"ms","BGN":"bg","BIF":"fr-BI","KHR":"km","XAF":"fr","CVE":"pt","KYD":"en-KY","CLP":"es-CL","COP":"es-CO","KMF":"ar","CDF":"fr-CD","NZD":"en-NZ","CRC":"es-CR","HRK":"hr","CUC":"es","ANG":"nl","CZK":"cs","DKK":"da","DJF":"fr-DJ","DOP":"es-DO","XCD":"en","EGP":"ar-EG","ERN":"ti-ET","ETB":"am","FJD":"en-FJ","GMD":"en","GEL":"ka","GHS":"ak","GIP":"en-GI","GTQ":"es-GT","GNF":"fr-GN","GYD":"en-GY","HTG":"fr-HT","HNL":"es-HN","HKD":"zh-Hant-HK","HUF":"hu","ISK":"is","INR":"hi","IDR":"id","IRR":"fa-IR","IQD":"ar-IQ","ILS":"he","JMD":"en-JM","JOD":"ar-JO","KZT":"kk","KES":"sw-KE","KPW":"ko-KP","KRW":"ko-KR","KWD":"ar-KW","KGS":"ky","LAK":"lo","LBP":"ar-LB","LSL":"st","LRD":"en-LR","LYD":"ar-LY","MOP":"zh-Hant","MKD":"mk","MGA":"mg","MWK":"ny","MYR":"ms-MY","MVR":"dv","MRU":"ar-MR","MUR":"fr-MU","MXN":"es-MX","MDL":"ro","MNT":"mn-Cyrl","MAD":"ar-MA","MZN":"pt-MZ","MMK":"my","NAD":"en-NA","NPR":"ne","NIO":"es-NI","NGN":"ig","NOK":"nb","OMR":"ar-OM","PKR":"ur","PAB":"es-PA","PGK":"tpi","PYG":"gn","PEN":"es-PE","PHP":"fil","PLN":"pl","QAR":"ar-QA","RON":"ro","RUB":"ru","RWF":"rw","SHP":"en-SH","WST":"sm","STN":"pt-ST","SAR":"ar-SA","RSD":"sr-Latn","SCR":"fr-SC","SLL":"kri","SGD":"zh-Hans-SG","SBD":"en-SB","SOS":"sw", "ZAR":"zu", "SSP":"ar-SS", "LKR":"si", "SDG":"ar-SD", "SRD":"nl-SR", "SZL":"en-SZ", "SEK":"sv-SE", "CHF":"fr-CH", "SYP":"ar-SY", "TWD":"zh-Hant-TW", "TJS":"tg", "TZS":"sw-TZ", "THB":"th", "TOP":"to", "TTD":"en-TT", "TND":"ar-TN", "TMT":"tk", "UGX":"sw-UG", "UAH":"uk", "AED":"ar-AE", "UYU":"es-UY", "UZS":"uz-Latn", "VUV":"bi", "VEF":"es-VE", "VND":"vi", "YER":"ar-YE", "ZMW":"en-ZM", "BWP":"en-BW", "CDF":"fr-CD" };
+    };
+
+    // bind to single element and change it's text
+    $.fn.tameCurrency = function(options = {}){
+        $(this).text(  __tameCurrencyFormat(options)  );
+        return;
+    };
+
+    $.tameCurrency = {
+        init:   __tameCurrencyFormatInit,
+        format: __tameCurrencyFormat,
+    };
+})(jQuery); 
